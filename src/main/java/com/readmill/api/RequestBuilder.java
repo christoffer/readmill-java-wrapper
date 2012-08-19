033126fb058da42ef5fde57b6ad5fd2fef7f13ef
Christoffer Klang <christoffer.klang@gmail.com>
2012-08-19 11:44:52 +0200

    Add base functionality for RequestBuilder

--- src/main/java/com/readmill/api/RequestBuilder.java
+++ src/main/java/com/readmill/api/RequestBuilder.java
@@ -1,4 +1,111 @@
 package com.readmill.api;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@SuppressWarnings("UnusedDeclaration")
+
 public class RequestBuilder {
+  private ReadmillWrapper mWrapper;
+  private Request mRequest;
+  private Class<? extends HttpRequestBase> mRequestBaseClass;
+
+  public <T extends HttpRequestBase> RequestBuilder(ReadmillWrapper wrapper, Class<T> requestBaseClass, String baseURI) {
+    mWrapper = wrapper;
+    mRequest = Request.to(baseURI);
+    mRequestBaseClass = requestBaseClass;
+  }
+
+  public Request getRequest() {
+    return mRequest;
+  }
+
+  public JSONArray jsonItems() {
+    return json().optJSONArray("items");
+  }
+
+  public JSONArray jsonItems(String unwrapKey) {
+    JSONArray items = json().optJSONArray("items");
+    ArrayList<JSONObject> unwrapped = new ArrayList<JSONObject>(items.length());
+    try {
+      for(int i = 0; i < items.length(); i++) {
+        unwrapped.add(items.getJSONObject(i).getJSONObject(unwrapKey));
+      }
+    } catch(JSONException e) {
+      e.printStackTrace();
+      return new JSONArray();
+    }
+    return new JSONArray(unwrapped);
+  }
+
+  public JSONObject json(String key) {
+    return json().optJSONObject(key);
+  }
+
+  /**
+   * Performs the request and returns the parsed JSON object.
+   */
+  public JSONObject json() {
+    try {
+      String responseText = getResponseText();
+      return new JSONObject(responseText);
+    } catch(JSONException e) {
+      e.printStackTrace();
+      return new JSONObject();
+    }
+  }
+
+  public String getResponseText() {
+    try {
+      HttpResponse response = sendRequest();
+      HttpEntity entity = response.getEntity();
+      return EntityUtils.toString(entity);
+    } catch(IOException e) {
+      e.printStackTrace();
+      return "";
+    }
+  }
+
+  public HttpResponse sendRequest() {
+    return mWrapper.execute(mRequest, mRequestBaseClass);
+  }
+
+  /************************/
+  /** Parameter builders **/
+  /************************/
+
+  /**
+   * Orders result before returning it from the server.
+   * Affects ranges.
+   */
+  public RequestBuilder order(String order) {
+    mRequest.withParams("order", order);
+    return this;
+  }
+
+  /**
+   * Filter on maximum highlight count
+   */
+  public RequestBuilder highlightsCountTo(int maxCount) {
+    mRequest.withParams("highlights_count[to]", maxCount);
+    return this;
+  }
+
+  /**
+   * Filter on minimum highlight count
+   */
+  public RequestBuilder highlightsCountFrom(int minCount) {
+    mRequest.withParams("highlights_count[from]", minCount);
+    return this;
+  }
+
+
 }
---
+++ src/test/java/com/readmill/api/RequestBuilderTest.java
@@ -0,0 +1,91 @@
+package com.readmill.api;
+
+import org.apache.http.client.methods.HttpGet;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class RequestBuilderTest {
+  RequestBuilder instance;
+  ReadmillWrapper mWrapper;
+
+  @Before
+  public void createInstance() {
+    Environment testEnvironment = new Environment("api.example.com", "example.com", true);
+    mWrapper = new ReadmillWrapper("client_id", "client_secret", testEnvironment);
+    instance = new RequestBuilder(mWrapper, HttpGet.class, "/users/1/readings");
+  }
+
+  @Test
+  public void getRequest() {
+    assertThat(instance.getRequest().toUrl(), containsString("/v2/users/1/readings"));
+  }
+
+  @Test
+  public void chainableParamBuilder() {
+    String queryString = instance.order("followers").highlightsCountFrom(50).getRequest().queryString();
+    assertThat(queryString, containsString("order=followers"));
+    assertThat(queryString, containsString("highlights_count%5Bfrom%5D=50"));
+  }
+
+  @Test
+  public void json() throws JSONException {
+    JSONObject user = new JSONObject("{ \"user\": { \"username\": \"christoffer\", id: 1 } }");
+    JSONObject json = builderWithStubbedResponseText(user.toString()).json();
+    assertThat(json.toString(), is(user.toString()));
+  }
+
+  @Test
+  public void jsonUnwrap() throws JSONException {
+    JSONObject user = new JSONObject("{ \"user\": { \"username\": \"christoffer\", id: 1 } }");
+    JSONObject json = builderWithStubbedResponseText(user.toString()).json("user");
+    assertThat(json.toString(), is(user.optJSONObject("user").toString()));
+  }
+
+  @Test
+  public void jsonItems() throws JSONException  {
+    JSONObject userOne = new JSONObject("{ \"user\": { \"username\": \"christoffer\", id: 1 } }");
+    JSONObject userTwo = new JSONObject("{ \"user\": { \"username\": \"niki\", id: 387 } }");
+
+    JSONObject usersWrapped = new JSONObject("{ \"items\": [ " + userOne.toString() + ", " + userTwo.toString() + " ]}");
+
+    JSONArray usersWithoutRoot = new JSONArray();
+    usersWithoutRoot.put(userOne);
+    usersWithoutRoot.put(userTwo);
+
+    JSONArray json = builderWithStubbedResponseText(usersWrapped.toString()).jsonItems();
+
+    assertThat(json.toString(), is(usersWithoutRoot.toString()));
+  }
+
+  @Test
+  public void jsonItemsUnwrap() throws JSONException  {
+    JSONObject userOne = new JSONObject("{ \"user\": { \"username\": \"christoffer\", id: 1 } }");
+    JSONObject userTwo = new JSONObject("{ \"user\": { \"username\": \"niki\", id: 387 } }");
+
+    JSONObject usersWrapped = new JSONObject("{ \"items\": [ " + userOne.toString() + ", " + userTwo.toString() + " ]}");
+
+    JSONArray usersUnwrapped = new JSONArray();
+    usersUnwrapped.put(userOne.getJSONObject("user"));
+    usersUnwrapped.put(userTwo.getJSONObject("user"));
+
+    JSONArray json = builderWithStubbedResponseText(usersWrapped.toString()).jsonItems("user");
+
+    assertThat(json.toString(), is(usersUnwrapped.toString()));
+  }
+
+  /** Private helpers **/
+
+  private RequestBuilder builderWithStubbedResponseText(String jsonText) throws JSONException {
+    JSONObject user = new JSONObject(jsonText);
+    RequestBuilder builder = Mockito.spy(instance);
+    Mockito.doReturn(user.toString()).when(builder).getResponseText();
+    return builder;
+  }
+}
