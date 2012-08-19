ec274053e6264a13389af9d5662141c1b7e97595
Christoffer Klang <christoffer.klang@gmail.com>
2012-08-19 20:51:57 +0200

    Allow specifying ports ni Environment

--- src/main/java/com/readmill/api/Environment.java
+++ src/main/java/com/readmill/api/Environment.java
@@ -7,15 +7,27 @@ public class Environment {
   private final HttpHost mApiHost, mLoginHost;
 
   /**
-   * Create an environment for a Api Wrapper
+   * Create an environment for a Api Wrapper.
    * @param apiHost Host to send requests to
    * @param loginHost Host where users can log in to the service
    * @param useSSL use secure connections or not
    */
   public Environment(String apiHost, String loginHost, boolean useSSL) {
+    this(apiHost, -1, loginHost, -1, useSSL);
+  }
+
+  /**
+   * Create an environment for a Api Wrapper.
+   * @param apiHost Host to send requests to
+   * @param apiPort Port of api host
+   * @param loginHost Host where users can log in to the service
+   * @param loginPort Port of login host
+   * @param useSSL use secure connections or not
+   */
+  public Environment(String apiHost, int apiPort, String loginHost, int loginPort, boolean useSSL) {
     String scheme = useSSL ? "https" : "http";
-    mApiHost = new HttpHost(apiHost, -1, scheme);
-    mLoginHost = new HttpHost(loginHost, -1, scheme);
+    mApiHost = new HttpHost(apiHost, apiPort, scheme);
+    mLoginHost = new HttpHost(loginHost, loginPort, scheme);
   }
 
   public HttpHost getApiHost() {
@@ -27,5 +39,4 @@ public class Environment {
   }
 
   public static final Environment Live = new Environment("api.readmill.com", "m.readmill.com", true);
-  public static final Environment Staging = new Environment("api.stage-readmill.com", "m.stage-readmill.com", false);
 }
