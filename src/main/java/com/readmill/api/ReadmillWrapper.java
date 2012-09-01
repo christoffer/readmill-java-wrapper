e6fc4683c7c597759ca36efa5c03709e3294f418
Christoffer Klang <christoffer.klang@gmail.com>
2012-09-01 23:55:42 +0200

    Add documentation comments for ReadmillWrapper

--- src/main/java/com/readmill/api/ReadmillWrapper.java
+++ src/main/java/com/readmill/api/ReadmillWrapper.java
@@ -37,7 +37,6 @@ public class ReadmillWrapper {
    * @param clientSecret Client secret
    * @param env          Server environment
    */
-
   public ReadmillWrapper(String clientId, String clientSecret, Environment env) {
     mClientId = clientId;
     mClientSecret = clientSecret;
@@ -103,7 +102,7 @@ public class ReadmillWrapper {
 
   /**
    * Set the scope for which to ask authorization.
-   *
+   * <p/>
    * Affects getAuthorizationURL() and obtainToken().
    *
    * @param scope scope to ask permission for
@@ -140,10 +139,36 @@ public class ReadmillWrapper {
   /**
    * Obtain a token by providing an authorization code.
    *
-   * @param authorizationCode Authorization code to exchange for a token
+   * @param authorizationCode Authorization code
    * @return The obtained token or null
+   * @see #obtainTokenThrows(String)
    */
   public Token obtainToken(String authorizationCode) {
+    try {
+      return obtainTokenThrows(authorizationCode);
+    } catch(IOException e) {
+      e.printStackTrace();
+    } catch(JSONException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  /**
+   * Obtain a token by providing an authorization code.
+   * <p/>
+   * Uses the redirect uri and scope set on the wrapper with #setRedirectURI()
+   * and #setScope().
+   * <p/>
+   * Obtaining a token only works for an authorization code that was requested
+   * with the same redirect uri and scope.
+   *
+   * @param authorizationCode The authorization code
+   * @return The obtained token
+   * @throws IOException if a network error occurs
+   * @throws JSONException if the response was not proper json
+   */
+  public Token obtainTokenThrows(String authorizationCode) throws IOException, JSONException {
     if(mRedirectURI == null) {
       throw new RuntimeException("Redirect URI must be set before calling obtainToken()");
     }
@@ -162,18 +187,16 @@ public class ReadmillWrapper {
       obtainRequest.withParams("scope", mScope);
     }
 
-    try {
-      String tokenResponse = getResponseText(obtainRequest, HttpPost.class);
-      JSONObject tokenJson = new JSONObject(tokenResponse);
-      return new Token(tokenJson);
-    } catch(IOException e) {
-      e.printStackTrace();
-    } catch(JSONException e) {
-      e.printStackTrace();
-    }
-    return null;
+    String tokenResponse = getResponseText(obtainRequest, HttpPost.class);
+    JSONObject tokenJson = new JSONObject(tokenResponse);
+    return new Token(tokenJson);
   }
 
+  /**
+   * Get the http client used to make requests.
+   *
+   * @return The HttpClient used for making requests with this wrapper
+   */
   public HttpClient getHttpClient() {
     if(mHttpClient == null) {
       HttpParams httpParams = new BasicHttpParams();
@@ -190,18 +213,42 @@ public class ReadmillWrapper {
     return mHttpClient;
   }
 
+  /**
+   * Start building a GET request to a given endpoint.
+   *
+   * @param uri Endpoint for the request
+   * @return a RequestBuilder for a request to the given endpint and verb
+   */
   public RequestBuilder get(String uri) {
     return new RequestBuilder(this, HttpGet.class, uri);
   }
 
+  /**
+   * Start building a POST request to a given endpoint.
+   *
+   * @param uri Endpoint for the request
+   * @return a RequestBuilder for a request to the given endpint and verb
+   */
   public RequestBuilder post(String uri) {
     return new RequestBuilder(this, HttpPost.class, uri);
   }
 
+  /**
+   * Start building a DELETE request to a given endpoint.
+   *
+   * @param uri Endpoint for the request
+   * @return a RequestBuilder for a request to the given endpint and verb
+   */
   public RequestBuilder delete(String uri) {
     return new RequestBuilder(this, HttpDelete.class, uri);
   }
 
+  /**
+   * Start building a PUT request to a given endpoint.
+   *
+   * @param uri Endpoint for the request
+   * @return a RequestBuilder for a request to the given endpint and verb
+   */
   public RequestBuilder put(String uri) {
     return new RequestBuilder(this, HttpPut.class, uri);
   }
@@ -210,44 +257,73 @@ public class ReadmillWrapper {
    * Protected
    */
 
-  protected HttpResponse execute(Request request, Class<? extends HttpRequestBase> klass) {
-    try {
-      authorizeRequest(request);
-      return getHttpClient().execute(resolveTarget(request), request.build(klass));
-    } catch(IOException e) {
-      e.printStackTrace();
-      return null;
-    }
+  /**
+   * Execute a request with a provided HTTP verb.
+   *
+   * @param request Request to execute
+   * @param klass   HTTP verb to use for request (HttpPost, HttpGet, etc.)
+   * @return The HttpResponse
+   * @throws java.io.IOException if a network error occurs
+   */
+  protected HttpResponse execute(Request request, Class<? extends HttpRequestBase> klass) throws IOException {
+    authorizeRequest(request);
+    return getHttpClient().execute(resolveTarget(request), request.build(klass));
   }
 
+  /**
+   * Execute a request and return the response body as a string.
+   *
+   * @param request Request to execute
+   * @param klass   HttpRequest class to execute as (HttpPost, HttpGet etc)
+   * @return The response body as a string
+   * @throws IOException if a network error occurs
+   */
   protected String getResponseText(Request request, Class<? extends HttpRequestBase> klass) throws IOException {
     HttpResponse response = execute(request, klass);
     return HttpUtils.getString(response);
   }
 
+  /**
+   * Add the strongest available authorization to a request.
+   * <p/>
+   * Order of authentication strengths (strongest to weakest)
+   * <p/>
+   * - Token assigned to the Request
+   * - Token assigned to the wrapper
+   * - Client id assigned to the wrapper
+   *
+   * @param request Request to authorize.
+   * @return The authorized request.
+   */
   protected Request authorizeRequest(Request request) {
     if(request.getToken() != null) {
-      return request; // Already authenticated with token
+      // Already authenticated with token
+      return request;
     }
 
     if(mToken != null) {
-      return request.usingToken(mToken); // Authenticate with wrapper token
+      // Authenticate with wrapper token
+      return request.usingToken(mToken);
     }
 
     if(mClientId != null) {
-      return request.withParams("client_id", mClientId); // Authenticate with client id
+      // Authenticate with client id
+      return request.withParams("client_id", mClientId);
     }
 
     return request;
   }
 
-  /*
-   * Private
-   */
+  // Private
 
+  /**
+   * Resolve the target host to use when executing HTTP requests.
+   *
+   * @param request The request to resolve target for
+   * @return The resolved HttpHost
+   */
   private HttpHost resolveTarget(Request request) {
     URI uri = URI.create(mEnvironment.getApiHost().toURI()).resolve(request.toUrl());
     return new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
   }
-
 }
