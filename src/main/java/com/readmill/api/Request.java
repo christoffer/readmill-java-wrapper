c2321ea755a3c323a7c9065855d7bd510ba74886
Christoffer Klang <christoffer.klang@gmail.com>
2012-09-02 00:27:50 +0200

    Add java doc for Request

--- src/main/java/com/readmill/api/Request.java
+++ src/main/java/com/readmill/api/Request.java
@@ -36,9 +36,29 @@ public class Request {
   private String mResource;
 
   @SuppressWarnings("UnusedDeclaration")
-  public Request() {
-  }
 
+  /**
+   * Constructs an empty request
+   */
+  public Request() {}
+
+  /**
+   * Constructs a request to a given resource, optionally formatted with any
+   * number of parameters.
+   *
+   * The following calls are equivalent:
+   *
+   * <code>
+   *   new Request("users/%d/readings", 1);
+   * </code>
+   * and
+   * <code>
+   *   new Request(String.format("users/%d/readings", 1));
+   * </code>
+   *
+   * @param resource Resource to request
+   * @param args (optional) objects used for formatting the resource
+   */
   public Request(String resource, Object... args) {
     if(args != null && args.length > 0) {
       resource = String.format(resource, args);
@@ -46,13 +66,28 @@ public class Request {
     mResource = resource;
   }
 
+  /**
+   * Constructs a request to a given resource, optionally formatted with any
+   * number of parameters.
+   *
+   * This method is just sugar for <code>new Request(String, Object...)</code>
+   *
+   * @param resource Resource to request
+   * @param args (optional) objects used for formatting the resource
+   * @return The constructed request.
+   */
   public static Request to(String resource, Object... args) {
     return new Request(resource, args);
   }
 
   /**
-   * @param args a list of arguments
-   * @return this
+   * Adds parameters to this request.
+   *
+   * The number of parameters has to given in pairs, but as many pairs as wanted
+   * can be added.
+   *
+   * @param args a list of arguments (must be even in length)
+   * @return this request
    */
   public Request withParams(Object... args) {
     if(args != null) {
@@ -68,10 +103,10 @@ public class Request {
   }
 
   /**
-   * The request should be made with a specific token.
+   * Sets the token used to authenticate this request.
    *
    * @param token the token
-   * @return this
+   * @return this request
    */
   public Request usingToken(Token token) {
     mToken = token;
@@ -79,7 +114,7 @@ public class Request {
   }
 
   /**
-   * Return the current token for this request.
+   * Returns the current token for this request.
    *
    * @return Request token or null
    */
@@ -88,26 +123,32 @@ public class Request {
   }
 
   /**
-   * @return a String that is suitable for use as an
-   *         <code>application/x-www-form-urlencoded</code> list of parameters
-   *         in an HTTP PUT or HTTP POST.
+   * Gets the query string for this request.
+   *
+   * The query string is the current parameters of this request in an
+   * <code>application/x-www-form-urlencoded</code> format.
+   *
+   * @return the query string
    */
   public String queryString() {
     return URLEncodedUtils.format(params, "UTF-8");
   }
 
   /**
-   * @return The request url (including querystring)
+   * Gets this request as an URL
+   *
+   * @return The url for this request (including query string)
    */
   public String toUrl() {
     return params.isEmpty() ? getResource() : getResource() + "?" + queryString();
   }
 
-  @Override
-  public String toString() {
-    return mResource == null ? queryString() : toUrl();
-  }
-
+  /**
+   * Builds a HttpRequest object based to make this request.
+   *
+   * @param klass HttpRequest class to use
+   * @return The (authorized) http request
+   */
   public <T extends HttpRequestBase> T build(Class<T> klass) {
     try {
       T request = klass.newInstance();
@@ -123,6 +164,12 @@ public class Request {
     }
   }
 
+  @Override
+  public String toString() {
+    return mResource == null ? queryString() : toUrl();
+  }
+
+  @SuppressWarnings("SimplifiableIfStatement")
   @Override
   public boolean equals(Object other) {
     if(other instanceof Request) {
@@ -140,6 +187,12 @@ public class Request {
    * Private *
    */
 
+  /**
+   * Attaches the params from this request to a HttpRequest
+   *
+   * @param request HttpRequest to attach params to
+   * @throws UnsupportedEncodingException if the param could not be serialized
+   */
   private void attachParams(HttpRequestBase request) throws UnsupportedEncodingException {
     if(request instanceof HttpEntityEnclosingRequestBase) {
       HttpEntityEnclosingRequestBase enclosingRequest = (HttpEntityEnclosingRequestBase) request;
@@ -153,12 +206,25 @@ public class Request {
     }
   }
 
+  /**
+   * Adds authentication to the provided HttpRequest
+   *
+   * @param request HttpRequest to authenticate
+   */
   private void authorizeRequest(HttpRequest request) {
     if(mToken != null && mToken.isValid()) {
       request.setHeader("Authorization", String.format("OAuth %s", mToken.getAccessToken()));
     }
   }
 
+  /**
+   * Gets the current resource.
+   *
+   * Relative resources are prefixed with /v2/. Absolute resources are left as
+   * is.
+   *
+   * @return The resource
+   */
   private String getResource() {
     if(mResource != null) {
       try {
