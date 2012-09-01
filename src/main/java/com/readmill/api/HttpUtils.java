b9720004348d7b61d8585d335565bdcb9f7e6a3a
Christoffer Klang <christoffer.klang@gmail.com>
2012-09-01 23:33:57 +0200

    Add documentation comments for HttpUtils

--- src/main/java/com/readmill/api/HttpUtils.java
+++ src/main/java/com/readmill/api/HttpUtils.java
@@ -8,16 +8,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+@SuppressWarnings("UnusedDeclaration")
 public class HttpUtils {
   /**
-   * Parses a http response as JSON.
+   * Consume a http response as JSON.
    *
    * @param response Response to parse.
-   * @return The parsed JSONObject or null
+   * @return The parsed JSONObject or null if it could not parsed or an
+   *         error occurred while consuming it.
    */
-  public static JSONObject optJson(HttpResponse response) {
+  public static JSONObject optJSON(HttpResponse response) {
     try {
-      return getJson(response);
+      return getJSON(response);
     } catch(IOException e) {
       e.printStackTrace();
       return null;
@@ -28,22 +30,36 @@ public class HttpUtils {
   }
 
   /**
-   * Returns a http response as JSON.
+   * Consume a http response as JSON.
    *
    * @param response Response to parse.
    * @return The parsed JSONObject
-   * @throws IOException
-   * @throws JSONException
+   * @throws IOException When an error occurred while consuming the response
+   * @throws JSONException When the response was not valid json
    */
-  public static JSONObject getJson(HttpResponse response) throws IOException, JSONException {
+  public static JSONObject getJSON(HttpResponse response) throws IOException, JSONException {
     return new JSONObject(getString(response));
   }
 
+  /**
+   * Consume a http response as a string.
+   *
+   * @param response The http response to consume
+   * @return The response body of the response as a string
+   * @throws IOException If there was an error consuming the response
+   */
   public static String getString(HttpResponse response) throws IOException {
     HttpEntity entity = response.getEntity();
     return EntityUtils.toString(entity);
   }
 
+  /**
+   * Consume a http response as a string.
+   *
+   * @param response The http response to consume
+   * @return The response body of the response as a string or null
+   *         if the http response could not be consumed.
+   */
   public static String optString(HttpResponse response) {
     try {
       return getString(response);
